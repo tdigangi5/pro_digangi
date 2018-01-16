bcc0d750c22b0d7b63d668888eea4d519a0de45a
Anthony DiGangi <digangi@Anthonys-MacBook-Air.local>
2018-01-15 21:03:08 -0500

    Adding the updated micro services

---
+++ inputform-backend/create.py
@@ -0,0 +1,41 @@
+import json
+import os
+import time
+import uuid
+import boto3
+dynamodb = boto3.resource('dynamodb')
+
+
+def create(event, context):
+    data = json.loads(event['body'])
+
+    timestamp = int(time.time() * 1000)
+
+    ## Creating a table inside DynamoDB
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    ## Items to populate the table
+    item = {
+        'id': str(uuid.uuid1()),
+        'firstName': data['firstName'],
+        'lastName': data['lastName'],
+        'email': data['email'],
+        'comments': data['comments'],
+        'createdAt': timestamp,
+        'updatedAt': timestamp
+    }
+
+    # write the item to the database
+    table.put_item(Item=item)
+
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(item),
+        "headers": {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": "true"
+        }
+    }
+
+    return response
